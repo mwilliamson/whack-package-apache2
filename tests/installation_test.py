40f9ce799e1be8e5af1ab561940a0fa9c0b2a024
Michael Williamson <mike@zwobble.org>
2012-12-27 20:11:37 +0000

    Build and install apache2 without any extras

---
+++ tests/installation_test.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+from nose.tools import istest, assert_equals
+from selenium import webdriver
+
+from tempdir import create_temporary_dir
+
+
+# TODO: select free port
+@istest
+def running_apache2_after_installation_shows_default_page():
+    with create_temporary_dir() as temp_dir:
+        install_apache2(temp_dir, port=54321)
+        process = start_apache2(temp_dir)
+        try:
+            assert_default_apache_page_is_visible(port=54321)
+        finally:
+            process.terminate()
+
+def install_apache2(directory, port):
+    path = os.path.join(os.path.dirname(__file__), "..")
+    subprocess.check_call(["whack", "install", path, directory, "-pport={0}".format(port), "--no-cache"])
+
+def start_apache2(temp_dir):
+    apache_dir = os.path.join(temp_dir, "apache2")
+    return subprocess.Popen(
+        ["bin/httpd", "-DNO_DETACH", "-d", apache_dir],
+        cwd=apache_dir
+    )
+
+def assert_default_apache_page_is_visible(port):
+    url = "http://localhost:{0}/".format(port)
+    browser = webdriver.Firefox()
+    try:
+        browser.get(url)
+        element = browser.find_element_by_css_selector("h1")
+        assert_equals("It works!", element.text)
+    finally:
+        browser.close()
