f5a15328d6354f84890075c8998a91ab3eaed7a0
Michael Williamson <mike@zwobble.org>
2012-12-28 17:46:40 +0000

    Fix HTTPD in apachectl

--- tests/installation_test.py
+++ tests/installation_test.py
@@ -7,23 +7,48 @@ from selenium import webdriver
 from tempdir import create_temporary_dir
 
 
+@istest
+def selenium_is_working():
+    browser = webdriver.Firefox()
+    browser.close()
+
+    
 # TODO: select free port
+# TODO: assert that apache has stopped
 @istest
 def running_apache2_after_installation_shows_default_page():
     with create_temporary_dir() as temp_dir:
         install_apache2(temp_dir, port=54321)
-        process = start_apache2(temp_dir)
+        process = subprocess.Popen(
+            ["bin/httpd", "-DNO_DETACH"],
+            cwd=temp_dir
+        )
         try:
             assert_default_apache_page_is_visible(port=54321)
         finally:
             process.terminate()
 
+
+@istest
+def apachectl_has_httpd_location_updated():
+    with create_temporary_dir() as temp_dir:
+        install_apache2(temp_dir, port=54321)
+        subprocess.check_call(
+            ["bin/apachectl", "start"],
+            cwd=temp_dir
+        )
+        try:
+            assert_default_apache_page_is_visible(port=54321)
+        finally:
+            subprocess.check_call(
+                ["bin/apachectl", "stop"],
+                cwd=temp_dir
+            )
+
+
 def install_apache2(directory, port):
     path = os.path.join(os.path.dirname(__file__), "..")
     subprocess.check_call(["whack", "install", path, directory, "-pport={0}".format(port), "--no-cache"])
-
-def start_apache2(temp_dir):
-    return subprocess.Popen(["bin/httpd", "-DNO_DETACH"], cwd=temp_dir)
 
 def assert_default_apache_page_is_visible(port):
     url = "http://localhost:{0}/".format(port)
