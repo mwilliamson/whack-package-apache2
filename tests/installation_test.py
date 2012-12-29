642f64de27b9f4a35eaa51a278bed09fa691ad38
Michael Williamson <mike@zwobble.org>
2012-12-29 18:12:53 +0000

    Don't allow port as a build parameter to improve caching

--- tests/installation_test.py
+++ tests/installation_test.py
@@ -46,7 +46,14 @@ def apachectl_has_httpd_location_updated():
 
 def install_apache2(directory, port):
     path = os.path.join(os.path.dirname(__file__), "..")
-    subprocess.check_call(["whack", "install", path, directory, "-pport={0}".format(port), "--no-cache"])
+    subprocess.check_call(["whack", "install", path, directory,"--no-cache"])
+    
+    conf_path = os.path.join(directory, "conf/httpd.conf")
+    with open(conf_path, "r") as conf_file:
+        original_conf_contents = conf_file.read()
+    conf_contents = original_conf_contents.replace("Listen 80", "Listen {0}".format(port))
+    with open(conf_path, "w") as conf_file:
+        conf_file.write(conf_contents)
 
 def assert_default_apache_page_is_visible(port):
     time.sleep(1)
